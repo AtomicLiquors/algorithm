50a575c412e883adc888d07cf9e67ebcb3f43c94
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2023-05-09 23:59:25 +0900

    hanoi

---
+++ DP/Main_1914.java
@@ -0,0 +1,53 @@
+package DP;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Main_1914 {
+    //메모리 초과
+    //깡 sysout이 멀쩡히 잘 통과하는 이유? https://devtitch.tistory.com/15
+
+    static int cnt = 0;
+    static StringBuilder sb;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] D = new int[N+1];
+
+        D[1] = 1;
+
+        if(N==1){
+            System.out.println(D[1]);
+            System.out.println("1 3");
+            return;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            D[i] = D[i-1]*2 + 1;
+        }
+        System.out.println(D[N]);
+        hanoi(1,3, 2, N);
+
+    }
+
+    static void hanoi(int start, int end, int mid, int n) throws IOException{
+        if(n==1){
+            //cnt++;
+            sb = new StringBuilder();
+            sb.append(start).append(" ").append(end);
+            bw.write(sb.toString());
+            bw.write("\n");
+            bw.flush();
+            return;
+        }
+
+        hanoi(start,mid, end,n-1);
+        hanoi(start,end,mid, 1);
+        hanoi(mid,end,start, n-1);
+    }
+}
